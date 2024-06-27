83b424a44d242a4aa2f93b3e8c30be09e67c9bec
Vivek Rajesh Joshi <vivekjoshi306@yahoo.com>
2024-06-27 23:57:02 +0530

    Update ReducedEchelonForm.lean

--- GaussElim/ReducedEchelonForm.lean
+++ GaussElim/ReducedEchelonForm.lean
@@ -1,29 +1,18 @@
 import Mathlib.Data.Matrix.Notation
-import Mathlib.Tactic.Linarith
 
-variable (F : Type) [Field F] [DecidableEq F] [Repr F] (M : Matrix (Fin m) (Fin n) Rat)
-
-/--
-Vector containing rows of the matrix as vectors
--/
-abbrev rowVecofMat := (Vector.ofFn M).map Vector.ofFn
+#check List
 
 /--
-Vector containing columns of the matrix as vectors
+The type of Vectors that would be rows in a row-reduced matrix with `n` columns.
+It is either filled with `0`s or of the form `[0,0,...,0,1,(arbitrary entries)]`
+
+If `row : ReducedRow n`:
+* `row.z` gives the leading zeros, or `none` if the row is all zeros
+* `row.k` gives the length of the arbitrary part
+* `row.tail` gives the arbitrary part
+* `row.h` is a proof that if the row isn't all zeros, then
+  `row.z` + `row.k` + 1 = n
 -/
-abbrev colVecofMat := (Vector.ofFn M.transpose).map Vector.ofFn
-
-/-Row-reduced form
-1. The first nonzero entry of every row must be 1
-2. Each column containing the first nonzero entry of a row must have all its other
-   entries as 0
--/
-
-def vector_isPrefixOf : Vector F k → Vector F k → Bool :=
-  fun v => fun w => List.isPrefixOf v.toList w.toList
-
-abbrev vec_allZero (v : Vector Rat k) : Bool := v.toList.all (fun x => (x==0))
-
 structure ReducedRow (n : Nat) where
   z : Option (Fin n)
   k : Nat
@@ -32,6 +21,9 @@ structure ReducedRow (n : Nat) where
 
 def zeroVec (k: Nat) : Vector Rat k := Vector.replicate k 0
 
+/--
+Form a Vector from the data stored in the `ReducedRow` type
+-/
 def ReducedRow.toVector (row : ReducedRow n): Vector Rat (n) :=
   match hz:row.z with
   | none => zeroVec n
@@ -40,38 +32,22 @@ def ReducedRow.toVector (row : ReducedRow n): Vector Rat (n) :=
 #eval ReducedRow.toVector (n:=5) ⟨some 2,2,⟨[3,4],rfl⟩,by simp⟩
 #eval ReducedRow.toVector (n:=4) ⟨none,_,⟨[],rfl⟩,by simp⟩
 
---padding : size of [0,0,...,0,1] pattern in a ReducedRow
-
--- inductive RowEchelonList : (padding : Nat) → (R : List (ReducedRow n)) → Type where
--- | nil : RowEchelonList 0 []
--- | cons : (row : ReducedRow n) → RowEchelonList padding R → row.z+1 > padding → RowEchelonList row.z (R.concat row)
-
--- def RowEchelonForm (m n : Nat) {padding : Nat} (R : List (ReducedRow n)) :=
---   { l : RowEchelonList padding R // R.length = m}
-
--- def RowEchelonForm.toMatrix {padding : Nat} (Q : RowEchelonForm m n R (padding:=padding)) : Matrix (Fin m) (Fin n) Rat :=
---   Q.2 ▸ List.length_map R _ ▸ (R.map fun r => r.toVector.get).get
-
--- def testl : List (ReducedRow 2) := [⟨0,_,⟨[2],rfl⟩,rfl⟩,⟨1,_,⟨[],rfl⟩,rfl⟩]
-
--- def r1 : ReducedRow 2 := ⟨0,_,⟨[2],rfl⟩,rfl⟩
--- def r2 : ReducedRow 2 := ⟨1,_,⟨[],rfl⟩,rfl⟩
-
--- def test1 : RowEchelonForm 1 2 [r1] (padding:=0) :=
---   ⟨RowEchelonList.cons r1 RowEchelonList.nil (Nat.zero_lt_succ _),rfl⟩
-
--- def testm : RowEchelonForm 2 2 testl (padding:=1) :=
---   ⟨RowEchelonList.cons r2 (RowEchelonList.cons r1 RowEchelonList.nil (Nat.zero_lt_succ _)) (by unfold r2 r1; simp),rfl⟩
-
--- #eval testm.toMatrix
-
 inductive RowEchelonList : (R : List (ReducedRow n)) → Type where
 | nil : RowEchelonList []
 | cons : (row : ReducedRow n) → RowEchelonList R → (∀ r ∈ R, (hz : r.z.isSome) → row.toVector.get (r.z.get hz) = 0) → RowEchelonList (R.concat row)
 
+/--
+A term of this type maps to an m×n matrix that satisies the following conditions:
+1. Every row must be row-reduced
+2. The leading one of every nonzero reduced row must have only zeros below it
+-/
 def RowEchelonForm (m n : Nat) (R : List (ReducedRow n)) :=
   { l : RowEchelonList R // R.length = m}
 
+/--
+Function to evaluate a term of type `RowEchelonForm m n R`
+as an m×n matrix that resembles R
+-/
 def RowEchelonForm.toMatrix (Q : RowEchelonForm m n R) : Matrix (Fin m) (Fin n) Rat :=
   Q.2 ▸ List.length_map R _ ▸ (R.map fun r => r.toVector.get).get
 
@@ -86,19 +62,14 @@ def test2 : RowEchelonForm 2 2 [r1,r2] :=
 
 #eval test2.toMatrix
 
-/-
-Row-reduced echelon form
-1. The matrix must be row-reduced
-2. All rows that have only 0s must occur before those that have a nonzero entry
-3. If rows 1,...,r are the nonzero rows and the first nonzero entry for each of
-these rows occurs in columns k₁,k₂,...,k_r, then  k₁< k₂<...< k_r
+/--
+A term of this type maps to a m×n matrix that satisfies the following conditions:
+1. Every row must be row-reduced
+2. The leading one of every nonzero reduced row must have only zeros above and below it
 -/
-
 inductive RowReducedEchelonList : (R : List (ReducedRow n)) → Type where
 | nil : RowReducedEchelonList []
-| cons : (row : ReducedRow n) → RowReducedEchelonList R → (∀ r ∈ R, (hz : r.z.isSome) → row.toVector.get (r.z.get hz) = 0) → ((R.map (fun r => r.z.map (fun p => r.toVector.get p))).all (fun x => x=some 0)) → RowReducedEchelonList (R.concat row)
-
---((R.map (fun r => r.z.map (fun p => r.toVector.get p))).all (fun x => x=some 0)) ???
+| cons : (row : ReducedRow n) → RowReducedEchelonList R → (∀ r ∈ R, (hz : r.z.isSome) → row.toVector.get (r.z.get hz) = 0) → ((hz : row.z.isSome) → (R.map fun r => r.toVector.get (row.z.get hz)).all (fun x => x=0)) → RowReducedEchelonList (R.concat row)
 
 def RowReducedEchelonForm (m n : Nat) (R : List (ReducedRow n)) :=
   { l : RowReducedEchelonList R // R.length = m}
@@ -118,9 +89,16 @@ def testm2 : RowReducedEchelonForm 2 2 [v1,v2] :=
 #eval testm2.toMatrix
 
 def a1 : ReducedRow 4 := ⟨some 0,_,⟨[0,4,0],rfl⟩,by simp⟩
-def a2 : ReducedRow 4 := ⟨some 1,_,⟨[1,3],rfl⟩,by simp⟩
+def a2 : ReducedRow 4 := ⟨some 1,_,⟨[3,0],rfl⟩,by simp⟩
 def a3 : ReducedRow 4 := ⟨some 3,_,⟨[],rfl⟩,by simp; rfl⟩
 def a4 : ReducedRow 4 := ⟨none,_,⟨[],rfl⟩,by simp⟩
 
 def m1 : RowReducedEchelonForm 4 4 [a1,a2,a3,a4] :=
-  ⟨RowReducedEchelonList.cons a4 (RowReducedEchelonList.cons a3 (RowReducedEchelonList.cons a2 (RowReducedEchelonList.cons a1 RowReducedEchelonList.nil))),rfl⟩
+  ⟨RowReducedEchelonList.cons a4 (RowReducedEchelonList.cons a3 (RowReducedEchelonList.cons a2 (RowReducedEchelonList.cons a1 RowReducedEchelonList.nil (by intro r hr1 hr2; simp at hr1) (by intro hz; simp)) (by intro r hr1 hr2; simp at hr1; simp [hr1,a2,a1,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.head]) (by intro hz; simp [a1,a2,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.get])) (by intro r hr1 hr2; simp at hr1; rcases hr1 with hr1|hr1 <;> simp [hr1,a3,a2,a1,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.head,Vector.get]) (by intro hz; simp [a1,a2,a3,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.get,List.get])) (by intro r hr1 hr2; simp at hr1; rcases hr1 with hr1|hr1|hr1 <;> simp [hr1,a4,a3,a2,a1,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.head,Vector.get,List.get]) (by intro hz; simp [a4] at hz),rfl⟩
+
+#eval m1.toMatrix
+
+def m2 : RowReducedEchelonForm 4 4 [a1,a2,a4,a3] :=
+  ⟨RowReducedEchelonList.cons a3 (RowReducedEchelonList.cons a4 (RowReducedEchelonList.cons a2 (RowReducedEchelonList.cons a1 RowReducedEchelonList.nil (by intro r hr1 hr2; simp at hr1) (by intro hz; simp)) (by intro r hr1 hr2; simp at hr1; simp [hr1,a2,a1,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.head]) (by intro hz; simp [a1,a2,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.get])) (by intro r hr1 hr2; simp at hr1; rcases hr1 with hr1|hr1 <;> simp [hr1,a4,a2,a1,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.head,Vector.get]) (by intro hz; simp [a4] at hz)) (by intro r hr1 hr2; simp at hr1; rcases hr1 with hr1|hr1|hr1; simp [hr1,a4,a3,a2,a1,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.head,Vector.get,List.get]; simp [hr1,a4,a3,a2,a1,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.head,Vector.get,List.get]; simp [hr1,a4] at hr2) (by intro hz; simp [a1,a2,a4,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.get,List.get]),rfl⟩
+
+#eval m2.toMatrix --not supposed to happen
