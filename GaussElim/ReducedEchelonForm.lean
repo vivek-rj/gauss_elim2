bb01ad16e7970e2f17c3f6a5bbc0dce82816edc6
Vivek Rajesh Joshi <vivekjoshi306@yahoo.com>
2024-06-28 12:14:48 +0530

    Update ReducedEchelonForm.lean

--- GaussElim/ReducedEchelonForm.lean
+++ GaussElim/ReducedEchelonForm.lean
@@ -1,6 +1,6 @@
 import Mathlib.Data.Matrix.Notation
 
-#check List
+abbrev colVecofMat (M : Matrix (Fin m) (Fin n) Rat) := (Vector.ofFn M.transpose).map Vector.ofFn
 
 /--
 The type of Vectors that would be rows in a row-reduced matrix with `n` columns.
@@ -34,7 +34,9 @@ def ReducedRow.toVector (row : ReducedRow n): Vector Rat (n) :=
 
 inductive RowEchelonList : (R : List (ReducedRow n)) → Type where
 | nil : RowEchelonList []
-| cons : (row : ReducedRow n) → RowEchelonList R → (∀ r ∈ R, (hz : r.z.isSome) → row.toVector.get (r.z.get hz) = 0) → RowEchelonList (R.concat row)
+| cons : (row : ReducedRow n) → RowEchelonList R → (∀ r ∈ R, (hz : r.z.isSome) → row.toVector.get (r.z.get hz) = 0) → (match R.getLast? with | none => true | some r => ¬(r.toVector = zeroVec n) ∨ row.toVector = zeroVec n) → RowEchelonList (R.concat row)
+
+--(match R with | [] => true | r::_ => ¬(r.toVector = zeroVec n) ∨ row.toVector = zeroVec n)
 
 /--
 A term of this type maps to an m×n matrix that satisies the following conditions:
@@ -55,12 +57,14 @@ def r1 : ReducedRow 2 := ⟨some 0,_,⟨[2],rfl⟩,by simp⟩
 def r2 : ReducedRow 2 := ⟨some 1,_,⟨[],rfl⟩,by simp⟩
 
 def test1 : RowEchelonForm 1 2 [r1] :=
-  ⟨RowEchelonList.cons r1 (RowEchelonList.nil) (by intro r hr1 hr2; simp at hr1),rfl⟩
+  ⟨RowEchelonList.cons r1 (RowEchelonList.nil) (by intro r hr1 hr2; simp at hr1) (rfl),rfl⟩
 
 def test2 : RowEchelonForm 2 2 [r1,r2] :=
-  ⟨RowEchelonList.cons r2 test1.1 (by intro r hr; unfold r2 ReducedRow.toVector zeroVec; unfold r1 at hr; simp at hr; simp [hr,Vector.replicate,Vector.append,Vector.head]),rfl⟩
+  ⟨RowEchelonList.cons r2 test1.1 (by intro r hr; unfold r2 ReducedRow.toVector zeroVec; unfold r1 at hr; simp at hr; simp [hr,Vector.replicate,Vector.append,Vector.head]) (by simp [r1,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector]),rfl⟩
 
 #eval test2.toMatrix
+
+#check List.last'
 
 /--
 A term of this type maps to a m×n matrix that satisfies the following conditions:
@@ -69,7 +73,7 @@ A term of this type maps to a m×n matrix that satisfies the following condition
 -/
 inductive RowReducedEchelonList : (R : List (ReducedRow n)) → Type where
 | nil : RowReducedEchelonList []
-| cons : (row : ReducedRow n) → RowReducedEchelonList R → (∀ r ∈ R, (hz : r.z.isSome) → row.toVector.get (r.z.get hz) = 0) → ((hz : row.z.isSome) → (R.map fun r => r.toVector.get (row.z.get hz)).all (fun x => x=0)) → RowReducedEchelonList (R.concat row)
+| cons : (row : ReducedRow n) → RowReducedEchelonList R → (∀ r ∈ R, (hz : r.z.isSome) → row.toVector.get (r.z.get hz) = 0) → ((hz : row.z.isSome) → (R.map fun r => r.toVector.get (row.z.get hz)).all (fun x => x=0)) → (match R.getLast? with | none => true | some r => ¬(r.toVector = zeroVec n) ∨ row.toVector = zeroVec n) → RowReducedEchelonList (R.concat row)
 
 def RowReducedEchelonForm (m n : Nat) (R : List (ReducedRow n)) :=
   { l : RowReducedEchelonList R // R.length = m}
@@ -81,10 +85,10 @@ def v1 : ReducedRow 2 := ⟨some 0,_,⟨[0],rfl⟩,by simp⟩
 def v2 : ReducedRow 2 := ⟨some 1,_,⟨[],rfl⟩,by simp⟩
 
 def testm1 : RowReducedEchelonForm 1 2 [v1] :=
-  ⟨RowReducedEchelonList.cons v1 RowReducedEchelonList.nil (by intro r hr; simp at hr) (by simp),rfl⟩
+  ⟨RowReducedEchelonList.cons v1 RowReducedEchelonList.nil (by intro r hr; simp at hr) (by simp) rfl,rfl⟩
 
 def testm2 : RowReducedEchelonForm 2 2 [v1,v2] :=
- ⟨RowReducedEchelonList.cons v2 testm1.1 (by unfold v1 v2; intro r hr; simp at hr; simp [hr,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.head]) (by unfold v1 v2 ReducedRow.toVector zeroVec; simp [Vector.replicate,Vector.append,Vector.get]),rfl⟩
+ ⟨RowReducedEchelonList.cons v2 testm1.1 (by unfold v1 v2; intro r hr; simp at hr; simp [hr,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.head]) (by unfold v1 v2 ReducedRow.toVector zeroVec; simp [Vector.replicate,Vector.append,Vector.get]) (by simp [v1,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector]),rfl⟩
 
 #eval testm2.toMatrix
 
@@ -94,11 +98,11 @@ def a3 : ReducedRow 4 := ⟨some 3,_,⟨[],rfl⟩,by simp; rfl⟩
 def a4 : ReducedRow 4 := ⟨none,_,⟨[],rfl⟩,by simp⟩
 
 def m1 : RowReducedEchelonForm 4 4 [a1,a2,a3,a4] :=
-  ⟨RowReducedEchelonList.cons a4 (RowReducedEchelonList.cons a3 (RowReducedEchelonList.cons a2 (RowReducedEchelonList.cons a1 RowReducedEchelonList.nil (by intro r hr1 hr2; simp at hr1) (by intro hz; simp)) (by intro r hr1 hr2; simp at hr1; simp [hr1,a2,a1,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.head]) (by intro hz; simp [a1,a2,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.get])) (by intro r hr1 hr2; simp at hr1; rcases hr1 with hr1|hr1 <;> simp [hr1,a3,a2,a1,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.head,Vector.get]) (by intro hz; simp [a1,a2,a3,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.get,List.get])) (by intro r hr1 hr2; simp at hr1; rcases hr1 with hr1|hr1|hr1 <;> simp [hr1,a4,a3,a2,a1,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.head,Vector.get,List.get]) (by intro hz; simp [a4] at hz),rfl⟩
+  ⟨RowReducedEchelonList.cons a4 (RowReducedEchelonList.cons a3 (RowReducedEchelonList.cons a2 (RowReducedEchelonList.cons a1 RowReducedEchelonList.nil (by intro r hr1 hr2; simp at hr1) (by intro hz; simp) rfl) (by intro r hr1 hr2; simp at hr1; simp [hr1,a2,a1,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.head]) (by intro hz; simp [a1,a2,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.get]) (by simp [a1,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector])) (by intro r hr1 hr2; simp at hr1; rcases hr1 with hr1|hr1 <;> simp [hr1,a3,a2,a1,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.head,Vector.get]) (by intro hz; simp [a1,a2,a3,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.get,List.get]) (by simp [a2,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector])) (by intro r hr1 hr2; simp at hr1; rcases hr1 with hr1|hr1|hr1 <;> simp [hr1,a4,a3,a2,a1,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.head,Vector.get,List.get]) (by intro hz; simp [a4] at hz) (by simp [a3,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector]),rfl⟩
 
 #eval m1.toMatrix
 
-def m2 : RowReducedEchelonForm 4 4 [a1,a2,a4,a3] :=
-  ⟨RowReducedEchelonList.cons a3 (RowReducedEchelonList.cons a4 (RowReducedEchelonList.cons a2 (RowReducedEchelonList.cons a1 RowReducedEchelonList.nil (by intro r hr1 hr2; simp at hr1) (by intro hz; simp)) (by intro r hr1 hr2; simp at hr1; simp [hr1,a2,a1,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.head]) (by intro hz; simp [a1,a2,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.get])) (by intro r hr1 hr2; simp at hr1; rcases hr1 with hr1|hr1 <;> simp [hr1,a4,a2,a1,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.head,Vector.get]) (by intro hz; simp [a4] at hz)) (by intro r hr1 hr2; simp at hr1; rcases hr1 with hr1|hr1|hr1; simp [hr1,a4,a3,a2,a1,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.head,Vector.get,List.get]; simp [hr1,a4,a3,a2,a1,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.head,Vector.get,List.get]; simp [hr1,a4] at hr2) (by intro hz; simp [a1,a2,a4,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.get,List.get]),rfl⟩
-
-#eval m2.toMatrix --not supposed to happen
+-- def m2 : RowReducedEchelonForm 4 4 [a1,a2,a4,a3] :=
+--   ⟨RowReducedEchelonList.cons a3 (RowReducedEchelonList.cons a4 (RowReducedEchelonList.cons a2 (RowReducedEchelonList.cons a1 RowReducedEchelonList.nil (by intro r hr1 hr2; simp at hr1) (by intro hz; simp) rfl) (by intro r hr1 hr2; simp at hr1; simp [hr1,a2,a1,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.head]) (by intro hz; simp [a1,a2,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.get]) (by simp [a1,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector])) (by intro r hr1 hr2; simp at hr1; rcases hr1 with hr1|hr1 <;> simp [hr1,a4,a2,a1,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.head,Vector.get]) (by intro hz; simp [a4] at hz) (by simp [a2,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector])) (by intro r hr1 hr2; simp at hr1; rcases hr1 with hr1|hr1|hr1; simp [hr1,a4,a3,a2,a1,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.head,Vector.get,List.get]; simp [hr1,a4,a3,a2,a1,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.head,Vector.get,List.get]; simp [hr1,a4] at hr2) (by intro hz; simp [a1,a2,a4,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector.get,List.get]) (by simp [a4,a3,ReducedRow.toVector,zeroVec,Vector.replicate,Vector.append,Vector]),rfl⟩
+-- --as expected
+-- #eval m2.toMatrix
